ba5a9247c64b2804061964cc84e505cb6c45de8e
Sonicizslow <77452076+Sonicizslow@users.noreply.github.com>
2023-04-19 23:27:23 +0000

    weheethshg

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -38,7 +38,7 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image}
-        onError={({ currentTarget }) => {
+        onError={( currentTarget ) => {
           this.onerror = null; // prevents looping
           this.src="/workspaces/Anythink-Market-clekt/frontend/src/components/ItemPreview.js"
         }} //added for comment
